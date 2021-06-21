f4796588d084307b37e14d8c7056a54edcc5d4fe
luckylovs <77058551+luckylovs@users.noreply.github.com>
2021-06-21 17:10:20 +0530

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,60 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+   question.hide();
+   background("yellow")
+   fill(0)
+   textSize(30)
+   text("Result of the Quiz",340,50)
+   text("------------------------",320,65)
+   Contestant.getPlayerInfo();
+   if(allcontestant !==undefined){
+     debugger;
+     var display_Answer = 230;
+     fill("blue")
+     textSize(20)
+     text("*Note: contestants who answered correct are highlighted in green color!",130,230)
+
+     for(var plr in allcontestant){
+       debugger;
+       var correctAns = "2"
+       if(correctAns === allcontestant[plr].answer)
+       fill("green")
+       else
+       fill("red")
+        display_Answer+=30
+        textSize(20)
+        text(allcontestant[plr].name + ":" + allcontestant[plr].answer,250,display_Answer)
+     }
+   }
+  }
+
+}
